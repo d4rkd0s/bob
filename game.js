8709e9900474bb76c577079b589c27e98121965f
Logan Schmidt <logo3801@gmail.com>
2015-02-02 20:19:45 -0600

    Debug text

--- game.js
+++ game.js
@@ -316,5 +316,17 @@ $(function() {
 
         } //update
     
+function render () {
+
+    if (game.scale.isFullScreen)
+    {
+        game.debug.text('ESC to leave fullscreen', 270, 16);
+    }
+    else
+    {
+        game.debug.text('Click / Tap to go fullscreen', 270, 16);
+    }
+
+}
 };
 });
