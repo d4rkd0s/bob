1e6ceee065d6867aa9fb3a9d81e4a9f627790ca1
airinhowl <airinhowl26@gmail.com>
2015-02-15 12:52:35 -0600

    apple Test

--- game.js
+++ game.js
@@ -168,7 +168,7 @@ $(function() {
         speed = 200+(10*score);
         
         //move mr.horse
-        if ( appleOnTree != 0 )
+        if ( appleOnTree = 1 )
         {
             horse.velocity = game.physics.arcade.accelerateToObject(horse, apple, 50+(score*5), 50+(score*5), 50+(score*5));
             if ( foot == 0 ){
